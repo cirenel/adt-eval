d6f5c120efbf0f7b726c5a6714397276bda8dd1d
claire luikart <5367081+cirenel@users.noreply.github.com>
2022-11-07 17:31:23 -0600

    adding delete, edit, add, search
    should work with cloud sql db

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from platform import release
+from wsgiref.util import request_uri
+from requests import session
+import requests
 import sqlalchemy
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
@@ -15,27 +18,29 @@ from google.cloud.sql.connector import Connector, IPTypes
 
 
 app = Flask(__name__)
-###############
-# configuration
-###############
+#################
+# configuration #
+#################
 
 #wow. so secure. much secret.
 app.config['SECRET_KEY'] = "sekrit"
 
 #instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
-
 #app.config["SQLALCHEMY_DATABASE_URI"]= f"postgresql+pg8000://{USERNAME}:{PASSWORD}@/{DBNAME}?unix_sock={db_socket_dir}/{PUBLIC_IP_ADDRESS}/.s.PGSQL.5432"
 #app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{USERNAME}:{PASSWORD}@localhost:5432/{DBNAME}" #there has to be a way to put this guy in google cloud :think:
 db_user =   "postgres"
 db_pass = "Windows2000"
 db_name = "nflix"
 project = "adt-eval:us-east1:adt-eval-nflix"  # e.g. '/cloudsql/project:region:instance'
 sql = "adt-eval-nflix"
 
-GOOGLE_APPLICATION_CREDENTIALS= '/cred.json' #this is real gross. 
+#this guy for local connect
+#app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_user}:{db_pass}@localhost:5432/{db_name}" #there has to be a way to put this guy in google cloud :think:
 
+#this guy for web deploy
+
+GOOGLE_APPLICATION_CREDENTIALS= '/cred.json' #this is real gross.
 def getconn():
     with Connector() as connector:
         conn = connector.connect(
@@ -53,24 +58,28 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "creator": getconn
 }
 
+
 Bootstrap(app)
 db = SQLAlchemy(app)
 with app.app_context():
     db.create_all()
 
 
-############
-#app routing
-############
+###############
+# app routing #
+###############
 
 @app.route("/",methods=['GET'])
 def index():
     time = datetime.now()
     #form options puller
     options = Entries.query.with_entities(Entries.rating).distinct()
-    print(options)
-    print(options[0], options[1], options[-1])
     form = StarterForm()
+    ratingOptions = Entries.query.with_entities(Entries.rating).distinct()
+    countryOptions = Entries.query.with_entities(Entries.country).distinct()
+    mediaTypeOptions = Entries.query.with_entities(Entries.media_type).distinct()
+    form.search.choices = mediaTypeOptions
+
     return render_template('index.html', time=time.strftime("%H:%M -- %d/%m/%y"), form=form)
 
 @app.route("/show<int:page>",methods=['GET'])
@@ -82,16 +91,63 @@ def showPage(page=1):
 
 @app.route("/search",methods=['GET', 'POST'])
 def search():
-    form=SearchForm()
-    return render_template('search.html', form=form)
+    if request.method == 'POST':
+        print("boop")
+        print(request.headers)
+        sqlQuery = "SELECT * FROM entries WHERE "
+        titleArg = (lambda:"entries.title LIKE '%'", lambda:"entries.title LIKE \'%"+request.form['mediaName']+"%\'")[request.form['mediaName']!=""]()
+        typeArg = (lambda:"entries.media_type LIKE '%'", lambda:"entries.media_type LIKE \'%"+request.form['mediaType']+"%\'")[request.form['mediaType']!=""]()
+        rateArg = (lambda:"entries.rating LIKE '%'", lambda:"entries.rating LIKE \'%"+request.form['rating']+"%\'")[request.form['rating']!=""]()
+        castArg = (lambda:"entries.cast_list LIKE '%'", lambda:"entries.cast_list LIKE \'%"+request.form['castList']+"%\'")[request.form['castList']!=""]()
+        dirArg = (lambda:"entries.director LIKE '%'", lambda:"entries.director LIKE \'%"+request.form['director']+"%\'")[request.form['director']!=""]()
+        countArg = (lambda:"entries.country LIKE '%'", lambda:"entries.country LIKE \'%"+request.form['country']+"%\'")[request.form['country']!=""]()
+        #print(">> "+countArg)
+        relYArg = (lambda:"entries.release_year LIKE '%'", lambda:"entries.release_year LIKE \'%"+request.form['yearReleased']+"%\'")[request.form['yearReleased']!=""]() #coming back a number. comp diff
+        durArg = (lambda:"entries.duration LIKE '%'", lambda:"entries.duration LIKE \'%"+request.form['runtime']+"%\'")[request.form['runtime']!=""]()
+
+      #  Arg = (lambda:"", lambda:"entries. LIKE \'%"+request.form['']+"%\'")[request.form['']!=""]()
+      #  Arg = (lambda:"", lambda:"entries. LIKE \'%"+request.form['']+"%\'")[request.form['']!=""]()
+        sqlQuery = sqlQuery+ titleArg+" AND "+typeArg+" AND "+rateArg+" AND "+castArg+" AND "+dirArg+" AND "+durArg+" AND "+countArg
+#        sqlQuery = "SELECT * FROM entries WHERE title LIKE \'%test%\'"
+        print(">> "+sqlQuery)
+        tab = db.session.execute(sqlQuery).all()
+        #print(len(tab))
+        return render_template('showSearch.html', table=tab, count=len(tab))
+    else:
+        form=SearchForm()
+        return render_template('search.html', form=form)
 
 @app.route("/add",methods=['GET', 'POST'])
 def addEntry():
+    if request.method=='POST':
+            sqlUpdate = "INSERT INTO entries (title, media_type, director, cast_list, country, release_year, rating,duration) VALUES (\'"+request.form['mediaName']+"\', \'"+request.form['mediaType']+"\',  \'"+request.form['director']+"\', \'"+request.form['castList']+"\', \'"+request.form['country']+"\',  \'"+request.form['yearReleased']+"\', \'"+request.form['rating']+"\', \'"+request.form['runtime']+"\'"
+            print(sqlUpdate)
+            db.session.execute(sqlUpdate)
+            db.session.commit()
+            with app.app_context():
+                db.create_all()
+            msg = "added record"
+            return render_template('index.html', time=msg)
     form=AddForm()
     return render_template('editEntry.html', form=form)
 
+@app.route("/delete", methods=['POST'])
+def deleteEntry():
+    entryPer = 10
+    #tab = db.session.execute(db.select(Entries).order_by(Entries.show_id)).paginate(page,entryPer)
+    tab = Entries.query.order_by(Entries.show_id).paginate(page=1, per_page=entryPer)
+    return render_template('show.html', table=tab)
+
 @app.route("/edit<string:show_id>",methods=['GET', 'POST'])
-def editEntry(show_id="s1"):
+def editEntry(show_id="s1", msg=""):
+    if request.method=='POST':
+        sqlUpdate = "UPDATE entries SET title= \'"+request.form['mediaName']+"\', media_type= \'"+request.form['mediaType']+"\', director= \'"+request.form['director']+"\', cast_list= \'"+request.form['castList']+"\',  country= \'"+request.form['country']+"\',   release_year= \'"+request.form['yearReleased']+"\',  rating= \'"+request.form['rating']+"\',  duration= \'"+request.form['runtime']+"\'  WHERE show_id=\'"+show_id+"\'"
+        print(sqlUpdate)
+        db.session.execute(sqlUpdate)
+        db.session.commit()
+        with app.app_context():
+            db.create_all()
+        msg = "updated record"
     form = EditForm()
     result = Entries.query.filter(Entries.show_id == show_id).all()
     print(show_id)
@@ -101,20 +157,20 @@ def editEntry(show_id="s1"):
     form.description.data = result[0].description
     form.genres.data = result[0].genre
     form.director.data = result[0].director
-    form.cast.data = result[0].cast_list
+    form.castList.data = result[0].cast_list
+    form.country.data = result[0].country
     form.rating.data = result[0].rating
     form.runtime.data = result[0].duration
     form.yearReleased.data = result[0].release_year
-    return render_template('editEntry.html', form=form, result=result)
-
+    return render_template('editEntry.html', form=form, result=result, msg=msg)
 
 def demoSel():
     result  = db.session.execute(db.select(Entries).order_by(Entries.show_id)).scalars()
     return result
 
-###########################
-#sqlalchemy supportive bits
-###########################
+########################
+# sqlalchemy db models #
+########################
 class Entries(db.Model):
     show_id = db.Column(db.String, primary_key=True)
     media_type = db.Column(db.String)
@@ -129,16 +185,17 @@ class Entries(db.Model):
     date_added  = db.Column(db.DateTime)
     release_year = db.Column(db.Integer)
 
-###########################
-# https://cloud.google.com/sql/docs/postgres/connect-app-engine-standard
-# friend documentation for connecting to cloud postgresql instance 
-###########################
-# connect_unix_socket initializes a Unix socket connection pool for
-# a Cloud SQL instance of Postgres.
+class Countries(db.Model):
+    country_id = db.Column(db.String, primary_key=True)
+    country = db.Column(db.String)
+
+class Genres(db.Model):
+    genre_id = db.Column(db.String, primary_key = True)
+    genre = db.Column(db.String)
 
 
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app)
+#from werkzeug.middleware.proxy_fix import ProxyFix
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- form.py
+++ form.py
@@ -1,14 +1,7 @@
+from requests import options
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField, TextAreaField, SelectMultipleField, BooleanField, RadioField
 
-
-class SearchForm(FlaskForm):
-    mediaName = StringField('Name')
-    minyear = StringField('min year')
-    maxyear = StringField('max year')
-    rating = StringField('rating')
-    runtime = StringField('runtime')
-    submit = SubmitField('Search')
 
 class FilterForm(FlaskForm):
     mediaName = StringField('Name')
@@ -16,48 +9,48 @@ class FilterForm(FlaskForm):
     #foreach distinct rating and genre, make a checkbox --> where get this and how subdivide?
     submit = SubmitField('Filter')
 
-class AddForm(FlaskForm):
-    mediaName = StringField('Name')
-    submit = SubmitField('Add')
 
 class EditForm(FlaskForm):
     mediaName = StringField('Name')
     mediaType = StringField('Type')
     runtime = StringField('Runtime')
-    description = StringField('Description')
     genres = StringField('Genre')
     rating = StringField('Rating')
     director = StringField('Director')
-    cast = StringField('Cast')
+    castList = StringField('Cast')
+    country = StringField('Country')
     yearReleased = StringField('Year')
+    description = TextAreaField('Description')
     submit = SubmitField('Edit')
 
 class AddForm(FlaskForm):
     mediaName = StringField('Name')
     mediaType = StringField('Type')
     runtime = StringField('Runtime')
-    description = StringField('Description')
     genres = StringField('Genre')
     rating = StringField('Rating')
     director = StringField('Director')
-    cast = StringField('Cast')
+    castList = StringField('Cast')
+    country = StringField('Country')
     yearReleased = StringField('Year')
+    description = TextAreaField('Description')
     submit = SubmitField('Add')
-    
+
 class SearchForm(FlaskForm):
     mediaName = StringField('Name')
     mediaType = StringField('Type')
     runtime = StringField('Runtime')
-    description = StringField('Description')
     genres = StringField('Genre')
     rating = StringField('Rating')
     director = StringField('Director')
-    cast = StringField('Cast')
+    castList = StringField('Cast')
+    country = StringField('Country')
     yearReleased = StringField('Year')
+    description = TextAreaField('Description')
     submit = SubmitField('Search')
 
 class StarterForm(FlaskForm):
     browse=SubmitField("Browse Listings")
-    search=SubmitField("Search Listings")
+    search=SelectField("Search Listings")
     edit=SubmitField("Edit Listings")
     filter=SubmitField("Filter Listings")
